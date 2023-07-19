9c10a130eb5c66633f26a143bcff5f8adbb341ab
Jari Nippula <jarix@ssrc.tii.ae>
2023-07-19 10:59:52 +0300

    ign-gz migration: misc

--- setup.bash
+++ setup.bash
@@ -1,4 +1,5 @@
 # !/bin/bash
 
-export IGN_GAZEBO_SYSTEM_PLUGIN_PATH=$1/Tools/sitl_ign_gazebo/build
-export IGN_GAZEBO_RESOURCE_PATH=$IGN_GAZEBO_RESOURCE_PATH:$1/Tools/sitl_ign_gazebo/models
+export GZ_GAZEBO_SYSTEM_PLUGIN_PATH=/usr/lib
+#export GZ_SIM_RESOURCE_PATH=$GZ_GAZEBO_RESOURCE_PATH:$1/Tools/sitl_ign_gazebo/models
+
